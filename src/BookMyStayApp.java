b86fc2b1d4868a9ae9f144a1c1562123efbc2182
vikki-05 <vikhyatatrivedi1020@gmail.com>
2026-03-20 09:03:39 +0530

    Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,23 @@
+/**
+ * Book My Stay - Hotel Booking System
+ *
+ * Entry point of the application
+ *
+ * @author Vikki
+ * @version 1.0
+ */.
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        String appName = "Book My Stay";
+        String version = "v1.0";
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to " + appName);
+        System.out.println(" Version: " + version);
+        System.out.println("=================================");
+
+        System.out.println("Application Started Successfully!");
+    }
+}
